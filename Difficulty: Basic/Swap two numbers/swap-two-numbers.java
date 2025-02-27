17060f163048f79b4146bae9bad941019a8188a7
Ravi Singh <116999956+RaviSingh48@users.noreply.github.com>
2025-02-27 10:12:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap two numbers/swap-two-numbers.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            Solution ob = new Solution();
+            List<Integer> ans = ob.get(a,b);
+            System.out.println(ans.get(0)+" "+ans.get(1));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static List<Integer> get(int a,int b)
+    {
+        // code here
+        int temp;
+        temp =  a;
+        a=  b;
+        b= temp;
+        return Arrays.asList(a, b);
+    }
+}
