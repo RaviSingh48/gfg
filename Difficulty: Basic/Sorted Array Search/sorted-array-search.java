5ae98e6b8b7d936c42e8284daeb4de6a632b1d51
Ravi Singh <116999956+RaviSingh48@users.noreply.github.com>
2025-03-17 21:16:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class gfg {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String inputLine[] = read.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int k = Integer.parseInt(read.readLine());
+
+            Solution obj = new Solution();
+
+            System.out.println(obj.searchInSorted(arr, k));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static boolean searchInSorted(int arr[], int k) {
+        int n=arr.length;
+        for(int i=0; i<n; i++){
+            if(arr[i]==k){
+                return true;
+            }
+        }
+        return false;
+        // Your code here
+    }
+}
